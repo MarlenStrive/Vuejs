cfb8ac1eb5b6effe78650fdd6e7c8187e6f5ba71
Maria Zeydina <maria.zeydina@dataart.com>
2018-10-04 19:37:01 +0300

    7 initial commit

--- 7/webpack.config.js
+++ 7/webpack.config.js
@@ -5,7 +5,7 @@ module.exports = {
   entry: './src/main.js',
   output: {
     path: path.resolve(__dirname, './dist'),
-    publicPath: '/6/vue-cli//dist/',
+    publicPath: '/7/dist/',
     filename: 'build.js'
   },
   module: {
